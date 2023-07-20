21d0ae20651e52f7680de0f74d64c9a6f3f619e1
Ritik Panday <panday.ritik@tftus.com>
2023-07-20 18:56:16 +0530

    auth api added

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,14 +3,14 @@ const express = require('express'); //import express
 const router = express.Router();
 const expenseController = require('../controllers/productController');
 const joi_validation = require('../services/joi_validation');
-
+const auth=require('../middleware/auth');
 router.post(
-  '/addProduct',
+  '/addProduct',auth,
   joi_validation.createExpenseValidation,
   expenseController.create
 );
 router.get('/seachItem', expenseController.seachItem);
-router.delete('/productDelete/:id', expenseController.productDelete);
-router.patch('/productUpdate/:id', expenseController.productUpdate);
+router.delete('/productDelete/:id', auth,expenseController.productDelete);
+router.patch('/productUpdate/:id', auth,expenseController.productUpdate);
 
 module.exports = router;
