9b6820602ea63ddfd2a66a40ff5ab8ec19b98ae3
Ritik Panday <panday.ritik@tftus.com>
2023-07-20 12:21:17 +0530

    finalchange

--- dbConnect/dbConnect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(mongoString,{useNewUrlParser: true})
-.then((res)=> console.log(">>>>>DB connected"))
-.catch((err)=> console.error("Connect fail"));
-
